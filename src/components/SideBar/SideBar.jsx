0aadf5cec6d377d0dd3ace8ec799ffab674fc90f
Alexandre Viretti <alexou73@gmail.com>
2021-12-03 10:26:48 +0100

    init and style of it

---
+++ src/components/SideBar/SideBar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+// Import assets
+import Logo from "../../assets/images/logo.png";
+
+const SideBar = () => {
+  return (
+    <div>
+      <ul class="sidebar">
+        <li class="sidebar-brand">
+          <img src={Logo} alt="" />
+          {/* <a href="">Welcome Mark</a> */}
+        </li>
+        <li className='sidebar__name'>
+          <a href="#">Welcome Mark</a>
+        </li>
+        <li>
+          <a href="#">Search SKU</a>
+        </li>
+        <li>
+          <a href="#">Search Store</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Logout</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default SideBar;
